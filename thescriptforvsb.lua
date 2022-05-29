e0cb80740121e65153bc22e3e8715ccd3164e8bf
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2022-05-29 17:49:40 +0100

    Update thescriptforvsb.lua

--- thescriptforvsb.lua
+++ thescriptforvsb.lua
@@ -5432,7 +5432,7 @@ local function require(func)
 	return func()
 end
 local function importvalue(v)
-	print("Loading: " ..v.Name)
+	print("Loading: " ..v.Name .." (note: if you are lagging thats normal, its loading alot of vlua)")
 	task.wait(.05)
 	return loadstring(v.Value)
 end
@@ -5549,6 +5549,8 @@ pcontrol.PlayerDraw = {}; pcontrol.PlayerDraw.BallTrail = importvalue(controlf.P
 						  pcontrol.PlayerDraw.Shield = importvalue(controlf.PlayerDraw.Shield)
 						  pcontrol.PlayerDraw.SpindashBall = importvalue(controlf.PlayerDraw.SpindashBall)
 						  pcontrol.PlayerDraw.init = importvalue(controlf.PlayerDraw.init)
+
+pcontrol.init = {}; pcontrol.init.client = importvalue(controlf.init.client)
 
 -- Run
 pcontrol.init.client()
